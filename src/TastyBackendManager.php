17cf201ee2d1386ff431519138d1b3b128e376bc
Jeni Tehan <jeni@deliciouscreative.com>
2017-07-06 18:35:06 +0100

    Add function to create admin views for content types, do that during module install.

--- src/TastyBackendManager.php
+++ src/TastyBackendManager.php
@@ -33,4 +33,42 @@ class TastyBackendManager extends SystemManager {
     return $output;
   }
 
+  /**
+   * Add a new administration view for a content type.
+   *
+   * @param Drupal\node\Entity\NodeType $type
+   *    Drupal NodeType object.
+   */
+  public function createAdminView($type) {
+
+    // Default view doesn't have any type set.
+    $type_filter = [
+      'id' => 'type',
+      'table' => 'node_field_data',
+      'field' => 'type',
+      'value' => [
+        $type->id() => $type->id(),
+      ],
+      'entity_type' => 'node',
+      'entity_field' => 'type',
+      'plugin_id' => 'bundle',
+      'group' => 1,
+    ];
+
+    // Override config from default view to create a new view.
+    $config = \Drupal::configFactory()->getEditable('views.view.tb_manage_content');
+    $config->setName('views.view.tb_manage_content_' . $type->id());
+    $config->set('label', 'Tasty Backend Manage ' . $type->label());
+    $config->set('id', 'tb_manage_content_' . $type->id());
+    $config->set('status', TRUE);
+    $config->set('description', 'Tasty Backend administration view to manage all ' . $type->label() . ' content');
+    $config->set('display.default.display_options.access.options.perm', 'edit any ' . $type->id() . ' content');
+    $config->set('display.default.display_options.filters.type', $type_filter);
+    $config->set('display.default.display_options.title', 'Manage ' . $type->label() . ' content');
+    $config->set('display.page_1.display_options.path', 'admin/manage/content/' . $type->id());
+    $config->set('display.page_1.display_options.menu.title', $type->label());
+    $config->set('display.page_1.display_options.menu.description', 'Manage ' . $type->label() . ' content.');
+    $config->save(TRUE);
+  }
+
 }
